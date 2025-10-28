cd3b9fe00185a03dc6826e1f57764d0baffce0a0
Paul- Emmanuel <paultchoume905@gmail.com>
2025-10-28 10:07:41 +0000

    Calculation feature

--- tip-calculator-app-main/js/app.js
+++ tip-calculator-app-main/js/app.js
@@ -1,22 +1,37 @@
 // Select elements
-let BillAmount = document.getElementById('bill');
-let numberOfPeople = document.getElementById('people');
-let customInput = document.getElementById('custom-tip');
-let tipPercentage = document.querySelectorAll('.buttons button');
-let tipAmount = document.getElementById('tip-amount');
-let totalAmount = document.getElementById('total-amount');
+const billAmount = document.getElementById('bill');
+const numberOfPeople = document.getElementById('people');
+const customInput = document.getElementById('custom-tip');
+const billTipAmount = document.getElementById('tip-amount');
+const billTotalPerPerson = document.getElementById('total-amount');
+const tipPercentage = document.querySelectorAll('.buttons button');
 
 // Attach event listeners
-tipPercentage.forEach(button => {
-    button.addEventListener('click', calculateTip);
+tipPercentage.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        let tipValue = e.target.innerText;
+        tipValue = tipValue.slice(0, -1);
+
+        if (billAmount.value === "") return;
+        if (numberOfPeople.value === "") numberOfPeople.value = 1;
+
+        calculateTip(
+            parseFloat(billAmount.value),
+            parseInt(tipValue),
+            parseInt(numberOfPeople.value)
+        );
+    });
 })
 
-function calculateTip (e) {
-    e.preventDefault();
+function calculateTip (billAmount, tipPercentage, numberOfPeople) {
+    let tipAmount = (billAmount * (tipPercentage / 100)) / numberOfPeople;
+    let tip = Math.floor(tipAmount * 100) / 100;
+    tip = tip.toFixed(2);
 
-    // Convert input values to numbers
-    BillAmount = parseFloat(BillAmount.value);
-    tipAmount = BillAmount * tipPercentage.value / 100;
-    totalAmount = BillAmount + tipAmount;
+    let totalAmount = (tipAmount * numberOfPeople + billAmount) / numberOfPeople;
+    totalAmount = totalAmount.toFixed(2);
+
+    billTipAmount.innerHTML = `$${tip}`;
+    billTotalPerPerson.innerHTML = `$${totalAmount}`
 }
 
